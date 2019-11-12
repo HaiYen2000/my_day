2e78cf5f89036066a99c09f32b340b8a066382f1
lamlt <yenchi2232000@gmail.com>
2019-11-12 23:52:23 +0700

    Add Intro slider and assets

---
+++ app/src/main/java/com/lamlt/my_day/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.lamlt.my_day;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            //getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+            supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+            getActionBar().hide();
+        }else{
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            //getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+            supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+            getSupportActionBar().hide();
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, IntroActivity.class));
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+
+
+}
